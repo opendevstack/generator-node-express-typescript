4e6625af5124859b9d6aa7966b30da4263317a58
Brooke Smith <brooke@oehmsmith.com>
2022-02-16 15:39:19 +1100

    generator-node-express-typescript - fixed dependencies and layout
    * Dependencies needed to be updated
    * Layout changed so can run tests when server is running
    * Fixed for tslint

--- generators/app/templates/src/index.ts
+++ generators/app/templates/src/index.ts
@@ -1,18 +1,14 @@
-export * from "./greeter";
+import * as bodyParser from "body-parser";
 import * as express from "express";
 import {Application} from "express";
-import bodyParser = require("body-parser");
 import weatherRouter from "./routes/weather";
-const app: Application = express();
 
+export * from "./greeter";
+
+const app: Application = express();
 app.use(bodyParser.json({type: "application/json"}));
 
 // register routes to listen to and expose
 app.use("/api/weather", weatherRouter);
 
-// initialize the webserver
-const host = process.env.YOUR_HOST || "0.0.0.0";
-const port = parseInt(process.env.PORT) || 8080;
-app.listen(port, host, () => {
-    console.info(`App listening on port ${port}!`);
-});
+export default app;
